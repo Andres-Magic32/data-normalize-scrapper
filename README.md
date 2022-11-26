8cdd841f68ce8437767f19ed0f57cf07908eaf28
Antonio_4 <andresl@finerioconnect.com>
2022-11-25 19:22:26 -0600

    Add project

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # data-normalize-scrapper
-Normalizador de datos de QA Scrapper a Excel para pruebas manuales.
+QA Scrapper data normalizer to Excel for manual testing.
+
+## How funtion
+The tool will allow you to map all the TRXs in an excel file in order to have
+A better user experience when performing manual tests.
+
+### What do you need? 
+Just setting name Json file in the index.js 
+
+Example
+
+In this case TRX_uno.json is the file name.
+
+const file = await readFile('./TRX_uno.json', 'utf-8');
---
+++ index.js
@@ -0,0 +1,78 @@
+import { readFile } from 'fs/promises'
+import xl from 'excel4node' // Se requiere para crear el excel
+
+const read = async() => {
+    // Obtiendo datos json
+    const file = await readFile('./TRX_uno.json', 'utf-8');
+    const json = JSON.parse(file)
+    // Apartir de aquí solo agrege los json que requiere unificar
+    const file_dos = await readFile('./TRX_dos.json', 'utf-8');
+    const json_dos = JSON.parse(file_dos)
+    const file_tres = await readFile('./TRX_03.json', 'utf-8');
+    const json_tres = JSON.parse(file_tres)
+    
+    // Tome los json y unifique las transacciones para tener un solo objeto de TRXS
+    let jsons = json.data.transactions.concat(json_dos.data.transactions, json_tres.data.transactions)    
+    
+
+    const wb = new xl.Workbook();
+    const ws = wb.addWorksheet('Sheet 1');
+    var style = wb.createStyle({
+        font: {
+          color: '#323136',
+          size: 14,
+        },       
+      });
+      
+      /**Asignando medidas de columna */
+      ws.column(1).setWidth(50);
+      ws.column(2).setWidth(60);
+      ws.column(3).setWidth(45);
+      ws.column(4).setWidth(25);
+      ws.column(5).setWidth(30);
+      ws.column(6).setWidth(30);
+      ws.column(7).setWidth(80);
+      ws.column(8).setWidth(10);
+      ws.column(9).setWidth(10);
+      ws.column(10).setWidth(15);
+      ws.column(11).setWidth(45);
+      ws.column(12).setWidth(15);
+      ws.row(9).setHeight(30);
+      
+      
+      ws.cell(9, 1).string('State').style(style)
+      ws.cell(9, 2).string('Account_id').style(style) 
+      ws.cell(9, 3).string('Credential_id').style(style) 
+      ws.cell(9, 4).string('TRX_id').style(style) 
+      ws.cell(9, 5).string('Unique_id').style(style) 
+      ws.cell(9, 6).string('create_at').style(style) 
+      ws.cell(9, 7).string('description').style(style) 
+      ws.cell(9, 8).string('status').style(style) 
+      ws.cell(9, 9).string('Is charge').style(style) 
+      ws.cell(9, 10).string('Amount').style(style) 
+      ws.cell(9, 11).string('Comentarios').style(style) 
+      ws.cell(9, 12).string('Is Wrong?').style(style) 
+
+      //Mapeando los datos en el excel. 
+      jsons.map((data,index)=> {        
+       index = index + 1    
+      ws.row(9 + index).setHeight(80);
+      ws.cell(9 + index, 1).string(json.data.state).style(style)
+      ws.cell(9 + index, 2).string(json.data.account_id).style(style) 
+      ws.cell(9 + index , 3).string(json.data.credential_id).style(style) 
+      ws.cell(9 + index, 4).string(data.id).style(style) 
+      ws.cell(9 + index, 5).string(data.extra_data.UniqueId).style(style)      
+      ws.cell(9 + index, 6).string(data.created_at).style(style) 
+      ws.cell(9 + index, 7).string(data.description).style(style) 
+      ws.cell(9 + index, 8).string(data.status).style(style) 
+      ws.cell(9 + index, 9).string(String(data.extra_data.IsCharge)).style(style) 
+      ws.cell(9 + index , 10).number(data.amount).style(style) 
+         
+      })
+
+      // Escribiendo el excel
+      wb.write('Excel.xlsx');
+}
+
+
+read() // Metodo void solo para ejecutar
