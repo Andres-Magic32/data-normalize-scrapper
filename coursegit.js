a642bc77b7bb63692577f589ec83e55fec65dd66
Andres-Magic32 <andresl@finerioconnect.com>
2022-12-14 21:37:23 -0600

    add a six tes

--- coursegit.js
+++ coursegit.js
@@ -2,3 +2,8 @@ const listaTestFive = () => {
     console.log('Lista test 5 since test5-lisa-amend')
     
 }
+
+const listaTestsix = () => {
+    console.log('Lista test 5 since test5-lisa-amend')
+    
+}
