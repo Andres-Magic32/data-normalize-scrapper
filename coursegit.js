2aeec13d3551c73cebf6c618faaaa3687fee3e7d
Andres-Magic32 <andresl@finerioconnect.com>
2022-12-14 22:19:59 -0600

    Add a nine test

--- coursegit.js
+++ coursegit.js
@@ -7,3 +7,8 @@ const listaTestsix = () => {
     console.log('Lista test 5 since test5-lisa-amend')
     
 }
+
+const listaTestnine = () => {
+    console.log('Lista test 9 since test5-lisa-amend')
+    
+}
